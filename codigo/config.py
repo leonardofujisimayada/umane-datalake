1614cff6a36fb3da3a4790bd33794e5c8173fb6c
Leonardo Fujisima <leonardo@fujisimaanalytics.com>
2025-12-09 10:55:28 -0300

    Busca dados da Monday - Funil

---
+++ codigo/config.py
@@ -0,0 +1,16 @@
+# ---------------------
+# IMPORTA BIBLIOTECAS - 
+# ---------------------
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Carrega dados do .env automaticamente
+
+# ------------
+# MONDAY API - 
+# ------------
+
+MONDAY_API_KEY = os.getenv("MONDAY_API_TOKEN")
+MONDAY_ENDPOINT = "https://api.monday.com/v2"
+
